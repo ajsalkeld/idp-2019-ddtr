3e6a4b93518fbe20b3144e84c5f1bdd39f9e7425
ajsalkeld <andrew@ajsalkeld.com>
2019-10-17 10:56:12 +0100

    fixed crashing bug

--- computer-vision/python-udp-comms/udp.py
+++ computer-vision/python-udp-comms/udp.py
@@ -8,15 +8,17 @@ UDP_IP = "192.168.137.180"    # arduino's IP
 UDP_PORT = 2390         # arduino's port
 MESSAGE = b"Hello, World!"
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
 # receive 
-LOCAL_IP = socket.gethostname()
-LOCAL_PORT = 5576
-#sock.bind((LOCAL_IP, LOCAL_PORT))
-print(LOCAL_IP)
+LOCAL_IP = ''    
+LOCAL_PORT = 5566
+address = (LOCAL_IP, LOCAL_PORT)
+sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+sock.bind(address)
+
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print("received message:", data)
