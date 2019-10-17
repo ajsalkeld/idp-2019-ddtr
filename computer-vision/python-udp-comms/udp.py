e0c7f8eab3c36c59da877e662fceee7904e82340
ajsalkeld <andrew@ajsalkeld.com>
2019-10-17 10:23:50 +0100

    UDP Python

--- computer-vision/python-udp-comms/udp.py
+++ computer-vision/python-udp-comms/udp.py
@@ -4,10 +4,20 @@
 
 import socket
 
-UDP_IP = "127.0.0.1"    # arduino's IP
-UDP_PORT = 2391         # arduino's port
-MESSAGE = "Hello, World!"
+UDP_IP = "192.168.137.180"    # arduino's IP
+UDP_PORT = 2390         # arduino's port
+MESSAGE = b"Hello, World!"
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+# receive 
+LOCAL_IP = "100.115.92.207"
+LOCAL_PORT = 8080
+sock.bind((LOCAL_IP, LOCAL_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print("received message:", data)
+
