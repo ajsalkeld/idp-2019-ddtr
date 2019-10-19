0d18be9076de469366c5c6a379b98a96a6458890
Benjamin Wilson <benjamin.jeremy.wilson@gmail.com>
2019-10-19 09:43:41 +0000

    Revert "Revert "Added UDP comms""
    This reverts commit 076163b9092e6748639059c779bf85462245b11d

--- computer-vision/main.py
+++ computer-vision/main.py
@@ -2,6 +2,7 @@ from global_stuff import *
 
 import mines
 import robot
+import udpComms
 from arena import *
 pt = Point
 
@@ -96,3 +97,5 @@ else:
 
 
     do_stuff(cv2.resize(img, tuple(RESOLUTION)))
+
+udpComms.start()
---
+++ computer-vision/udpComms.py
@@ -0,0 +1,34 @@
+from global_stuff import *
+
+def setup():
+    # bind all IP
+    HOST = '0.0.0.0' 
+    # Listen on Port 
+    PORT = 44444 
+    #Size of receive buffer   
+    BUFFER_SIZE = 1024    
+    # Create a TCP/IP socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Bind the socket to the host and port
+    s.bind((HOST, PORT))
+    # Start thread for receiving data
+    rxthread = threading.Thread(target=rxThread)
+    rxthread.start()
+
+def rxThread():
+    s.sendto(b"Hello from python", ("192.168.43.82", 2390))
+    while True:
+        # Receive BUFFER_SIZE bytes data
+        # data is a list with 2 elements
+        # first is data
+        #second is client address
+        data = s.recvfrom(BUFFER_SIZE)
+        if data:
+            # print received data
+            print('Client to Server: ' , data)
+            # Message is contained in data[0]
+    # Close connection
+    s.close()
+
+def sendCommand(command):
+    s.sendto(command, ("192.168.43.82", 2390))
