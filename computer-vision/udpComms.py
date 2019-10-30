688355f8555105c62157edb182c122bb403651ce
Ben <bw419@cam.ac.uk>
2019-10-30 10:44:12 +0000

    better distortion correction, initial communication from nina to computer

--- computer-vision/udpComms.py
+++ computer-vision/udpComms.py
@@ -1,23 +1,37 @@
 from global_stuff import *
+import json
 
-CURR_UDP_DATA = "none"
+CURR_UDP_DATA = {}
 
 def rxThread():
     s.sendto(b"Hello from python", (IP, 2390))
-    try:
+    # try:
         while True:
             global CURR_UDP_DATA
             # Receive BUFFER_SIZE bytes data
             # data is a list with 2 elements
             # first is data
             # second is client address
-            CURR_UDP_DATA = str(s.recvfrom(BUFFER_SIZE))
+            new_data = s.recvfrom(BUFFER_SIZE)
+            if new_data:
+                new_data = "{" + new_data[0].decode() + "}"
+
+                try:
+                    parsed = json.loads(new_data)
+                    
+                    CURR_UDP_DATA = parsed
+                    print(CURR_UDP_DATA)
+
+
+                except json.decoder.JSONDecodeError:
+                    print("failed to decode json: ", new_data)
             # if data:
                 ## print received data
                 # print('Client to Server: ' , data)
                 ## Message is contained in data[0]
         # Close connection
-    except:
+    # except Exception as e:
+        print(e)
         print("stopping udp comms thread")
         sendCommand(b"stop")
         s.close()
@@ -25,7 +39,7 @@ def rxThread():
 def sendCommand(command):
     s.sendto(command, (IP, 2390))
 
-IP = "192.168.137.38"
+IP = "192.168.137.110"
 
 # def setup():
 # bind all IP
@@ -41,7 +55,9 @@ s.bind((HOST, PORT))
 # Start thread for receiving data
 
 if __name__ == "__main__":
-    sendCommand(b"stop")
+    # sendCommand(b"stop")
+    rxthread = threading.Thread(target=rxThread)
+    rxthread.start()
 elif USE_VIDEO:
     rxthread = threading.Thread(target=rxThread)
     rxthread.start()
